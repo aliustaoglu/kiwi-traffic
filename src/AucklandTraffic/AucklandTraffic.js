4ff9398e000456682a0adf18c653a3a55257e2b6
Cuneyt Aliustaoglu <aliustaoglu@yahoo.com>
2019-07-19 15:26:31 +1200

    get highway congestion info

--- src/AucklandTraffic/AucklandTraffic.js
+++ src/AucklandTraffic/AucklandTraffic.js
@@ -6,35 +6,68 @@ import axios from 'axios'
 import { aucklandTrafficConditions, aucklandHeaders } from '../api/endpoints'
 import { parseString } from 'react-native-xml2js'
 
-let k = 0
+const getTrafficData = result => {
+    const lastUpdated = result['tns:getTrafficConditionsResponse']['tns:trafficConditions'][0]['tns:lastUpdated']
+    let traffic = []
+    result['tns:getTrafficConditionsResponse']['tns:trafficConditions'][0]['tns:motorways'].map(motorway => {
+        motorway['tns:locations'].map(location => {
+            traffic.push({
+                congestion: location['tns:congestion'][0],
+                direction: location['tns:direction'][0],
+                endLat: location['tns:endLat'][0],
+                endLon: location['tns:endLon'][0],
+                id: location['tns:id'][0],
+                inOut: location['tns:inOut'][0],
+                name: location['tns:name'][0],
+                order: location['tns:order'][0],
+                startLat: location['tns:startLat'][0],
+                startLon: location['tns:startLon'][0],
+                motorway: motorway['tns:name'][0]
+            })
+        })
+    })
+    return {
+        lastUpdated,
+        traffic
+    }
+}
 
 class AucklandTraffic extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      latLng: {
-        lat: -36.8485,
-        lng: 174.7633
-      }
+    constructor(props) {
+        super(props)
+        this.state = {
+            latLng: {
+                lat: -36.8485,
+                lng: 174.7633
+            },
+            data: {}
+        }
+        this.onMapReady = this.onMapReady.bind(this)
     }
-  }
 
-  async componentDidMount () {
-    /* const xmlAkl = await axios.get(aucklandTrafficConditions, { headers: aucklandHeaders })
-    const responseAkl = parseString(xmlAkl.data, function (err, result) {
-      console.dir(result)
-    }) */
-  }
+    async onMapReady() {
+        const xmlAkl = await axios.get(aucklandTrafficConditions, { headers: aucklandHeaders })
+        const that = this
+        parseString(xmlAkl.data, function(err, result) {
+            const trafficData = getTrafficData(result)
+            that.setState({ trafficData })
+            console.log(trafficData)
+            const heavy = trafficData.traffic.filter( t => t.congestion === 'Heavy' )
+            console.log(heavy)
+        })
+    }
 
-  render () {
-    return (
-      <SafeAreaView style={{ height: '100%' }}>
-        <Layout style={{ height: '100%' }}>
-          <MapView latLng={this.state.latLng} zoom={12} onMapReady={() => console.log('MAPREADY')} />
-        </Layout>
-      </SafeAreaView>
-    )
-  }
+    async componentDidMount() {}
+
+    render() {
+        return (
+            <SafeAreaView style={{ height: '100%' }}>
+                <Layout style={{ height: '100%' }}>
+                    <MapView latLng={this.state.latLng} zoom={12} onMapReady={this.onMapReady} />
+                </Layout>
+            </SafeAreaView>
+        )
+    }
 }
 
 export default AucklandTraffic
