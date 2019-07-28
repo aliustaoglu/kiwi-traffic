1a51e593bc098337fe54e0b0bd8be7b491909059
Cuneyt Aliustaoglu <aliustaoglu@yahoo.com>
2019-07-28 22:26:55 +1200

    Istanbul test coverage for jest and sonarqube

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: ['node_modules/(?!react-native|react-navigation)/'],
-  setupFiles: ['./setupTests.js']
+  setupFiles: ['./setupTests.js'],
+  testResultsProcessor: 'jest-sonar-reporter',
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "target",
+    "build",
+    "coverage",
+    "sonar-report",
+    "node"
+]
 }
