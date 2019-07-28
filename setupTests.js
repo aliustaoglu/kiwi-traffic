7588e731ca27db8c6c8e2298aeb3f41b3db07dc3
Cuneyt Aliustaoglu <aliustaoglu@yahoo.com>
2019-07-28 19:23:30 +1200

    Jest config

---
+++ setupTests.js
@@ -0,0 +1,15 @@
+import { NativeModules } from 'react-native'
+// https://github.com/kmagiera/react-native-gesture-handler/issues/344
+Object.assign(NativeModules, {
+  RNGestureHandlerModule: {
+    attachGestureHandler: jest.fn(),
+    createGestureHandler: jest.fn(),
+    dropGestureHandler: jest.fn(),
+    updateGestureHandler: jest.fn(),
+    State: {},
+    Directions: {}
+  },
+  PlatformConstants: {
+    forceTouchAvailable: false
+  }
+})
